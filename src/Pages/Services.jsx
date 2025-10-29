0f371a9343f01b6d8b68bec1ca3f871997538bc6
Alex Linares <alexlinares0209@gmail.com>
2025-10-29 04:19:34 -0500

    index css y services modificado

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -4,45 +4,45 @@ import { Link } from "react-router-dom"
 const Services = () => {
   return (
     <div className="services">
-        <h2>Mis Servicios</h2>
-        <p>Ofrezco una gama completa de servicios de arquitectura para satisfacer las necesidades de mis clientes, desde el diseño conceptual 
+        <h2 className="animate__animated animate__slideInDown">Mis Servicios</h2>
+        <p className="animate__animated animate__slideInLeft">Ofrezco una gama completa de servicios de arquitectura para satisfacer las necesidades de mis clientes, desde el diseño conceptual 
         hasta la gestión de proyectos. Mi enfoque se centra en la creación de espacios funcionales, estéticos y sostenibles que reflejan la visión
         y personalidad de cada cliente.</p>
 
         <div className="services-container">
-            <div className="service-card">
+            <div className="service-card animate__animated animate__slideInRight">
                 <i className="fa-solid fa-pen-ruler"></i>
                 <h3>Diseño Arquitectónico</h3>
                 <p>Creación de diseños arquitectónicos innovadores y funcionales para proyectos residenciales, comerciales e institucionales.</p>
             </div>
-            <div className="service-card">
+            <div className="service-card animate__animated animate__slideInLeft">
                 <i className="fa-solid fa-couch"></i>
                 <h3>Diseño de Interiores</h3>
                 <p>Diseño de interiores que combina estética y funcionalidad para crear espacios acogedores y personalizados.</p>
             </div>
-            <div className="service-card">
+            <div className="service-card animate__animated animate__slideInRight">
                 <i className="fa-solid fa-lightbulb"></i>
                 <h3>Consultoría</h3>
                 <p>Asesoramiento experto en todas las fases del proyecto, desde la viabilidad hasta la selección de materiales.</p>
             </div>
-            <div className="service-card">
+            <div className="service-card animate__animated animate__slideInLeft">
                 <i className="fa-solid fa-list-check"></i>
                 <h3>Gestión de Proyectos</h3>
                 <p>Gestión integral de proyectos para garantizar que se cumplan los plazos, el presupuesto y la calidad.</p>
             </div>
-            <div className="service-card">
+            <div className="service-card animate__animated animate__slideInRight">
                 <i className="fa-solid fa-cube"></i>
                 <h3>Visualización 3D</h3>
                 <p>Creación de representaciones visuales realistas en 3D para visualizar el proyecto antes de su construcción.</p>
             </div>
-            <div className="service-card">
+            <div className="service-card animate__animated animate__slideInLeft">
                 <i className="fa-solid fa-house"></i>
                 <h3>Urbanismo</h3>
                 <p>Planificación y diseño de espacios urbanos sostenibles y funcionales que mejoren la calidad de vida.</p>
             </div>
         </div>
 
-        <div className="contact-container">
+        <div className="contact-container animate__animated animate__slideInRight">
             <h3>Contáctame para tu próximo proyecto</h3>
             <p>Estoy lista para ayudarte a hacer realidad tus ideas. Solicita más información o un presupuesto sin compromiso.</p>
             <Link to={"/contact"} className="request-link">Solicitar más información</Link>
